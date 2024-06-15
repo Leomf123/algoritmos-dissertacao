104fb6ba4257007bcf36df128d980aa5fd0e75f9
Leonardo Macedo Freire <lmacedo.freire@gmail.com>
2024-06-14 23:41:14 -0300

    refactor: Otimizar função retirar rotulos

--- src/processar_rotulos.py
+++ src/processar_rotulos.py
@@ -7,30 +7,30 @@ from sklearn.metrics import balanced_accuracy_score, f1_score
 # saída: vertor numpy de rotulos com alguns não rotulados
 def retirar_rotulos(rotulos, porcentagem_manter, classes, seed = 100):
  
- random.seed(seed)
- 
- quantidade_retirar = int(len(rotulos)*(1 - porcentagem_manter))
- if len(rotulos)-quantidade_retirar < len(classes):
-   quantidade_retirar = len(rotulos)-len(classes)
+  random.seed(seed)
+  
+  quantidade_retirar = int(len(rotulos)*(1 - porcentagem_manter))
+  if len(rotulos)-quantidade_retirar < len(classes):
+    quantidade_retirar = len(rotulos)-len(classes)
 
- rotulos_semissupervisionado = np.array(rotulos)
- 
- # Garante que guardo pelo menos 1 de cada classe
- posicoes_protegidas = np.zeros(len(classes))
- for i in range(len(classes)):
-   posicoes = [j for j, v in enumerate(rotulos) if v == classes[i]]
-   posicao_aleatoria = random.randint(0, len(posicoes)-1)
-   posicoes_protegidas[i] = posicoes[posicao_aleatoria]
- 
- for i in range(quantidade_retirar):
-  while(True):
-    posicao_aleatoria = random.randint(0, len(rotulos)-1)
-    if rotulos_semissupervisionado[posicao_aleatoria] != 0 and not posicao_aleatoria in posicoes_protegidas:
-      break
+  rotulos_semissupervisionado = np.array(rotulos)
+  
+  # Garante que guardo pelo menos 1 de cada classe
+  posicoes_protegidas = np.zeros(len(classes))
+  for i in range(len(classes)):
+    posicoes = [j for j, v in enumerate(rotulos) if v == classes[i]]
+    posicao_aleatoria = random.randint(0, len(posicoes)-1)
+    posicoes_protegidas[i] = posicoes[posicao_aleatoria]
+  
+  posicoes_aleatorias = np.array([i for i in range(len(rotulos))])
+  posicoes_aleatorias = np.setdiff1d(posicoes_aleatorias, posicoes_protegidas)
+    
+  posicoes_aleatorias = np.array(random.sample(posicoes_aleatorias.tolist(), quantidade_retirar))
+  
+  if len(posicoes_aleatorias) > 0:
+    rotulos_semissupervisionado[posicoes_aleatorias] = 0
 
-  rotulos_semissupervisionado[posicao_aleatoria] = 0
-
- return rotulos_semissupervisionado
+  return rotulos_semissupervisionado
 
 
 def acuracia(rotulos_originais, rotulos_propagados,rotulos_semissupervisionado):
