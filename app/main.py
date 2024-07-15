abc5c1cc2a464db8ae6863b7ab0a285255a74a36
Leonardo Macedo Freire <lmacedo.freire@gmail.com>
2024-07-15 14:02:30 -0300

    chore: Inicializa testes em main

--- app/main.py
+++ app/main.py
@@ -1,152 +1,65 @@
-import numpy as np
-import pandas as pd
-import random
-import time
+import sys
+import os
 
-from utils import gerar_matriz_distancias
-from algoritmos_adjacencias import gerar_matriz_adjacencias
-from algoritmos_peso import gerar_matriz_pesos
-from processar_rotulos import retirar_rotulos, medidas_qualidade
-from algoritmos_classificar import propagar
-from utils import ordem_rotulos_primeiro, divisao_L, gravar_resultados, definir_medida_distancia
-from utils import normalizar_dados, retornar_sigma, retornar_omega, checar_matrix_adjacencias
-from processar_rotulos import one_hot
+# Caminho absoluto para o diretório que contém o módulo
+caminho_diretorio = os.path.join(os.path.dirname(__file__), 'algoritmos-dissertacao', 'app')
+# Adiciona o diretório ao sys.path
+sys.path.append(caminho_diretorio)
 
-datasets = [
-    "ace_ECFP_4.data",
-    "ace_ECFP_6.data",
-    "analcatdata_authorship-458.data",
-    "armstrong2002v1.data",
-    "articles_1442_5.data",
-    "articles_1442_80.data",
-    "autoPrice.data",
-    "banknote-authentication.data",
-    "cardiotocography.data",
-    "chowdary2006.data",
-    "chscase_geyser1.data",
-    "cox2_ECFP_6.data",
-    "dhfr_ECFP_4.data",
-    "dhfr_ECFP_6.data",
-    "diggle_table.data",
-    "fontaine_ECFP_4.data",
-    "fontaine_ECFP_6.data",
-    "gordon2002.data",
-    "iris.data",
-    "m1_ECFP_4.data",
-    "m1_ECFP_6.data",
-    "mfeat-factors.data",
-    "mfeat-karhunen.data",
-    "seeds.data",
-    "segmentation-normcols.data",
-    "semeion.data",
-    "stock.data",
-    "transplant.data",
-    "wdbc.data",
-    "wine-187.data",
-    "yeast_Galactose.data"
-]
+# Agora podemos importar o módulo diretamente
+from teste import teste
 
-K = [2, 4, 6, 8, 10, 12, 14, 16]
 
-Adjacencia = ["mutKNN", "symKNN", "symFKNN", "MST"]
-
-Ponderacao = ["RBF", "HM", "LLE"]
-
-Quantidade_rotulos = [0.02, 0.05, 0.08, 0.1]
-
-Quantidade_experimentos = 30
-
-Propagacao = ["GRF", "RMGT", "LGC", "LapRLS", "LapSVM"]
-
-test_ID = 0
-
-inicio_geral = time.time()
-# 1 - Para cada dataset
-for nome_dataset in datasets:
-    inicio = time.time()
-    print("Datset: ", nome_dataset)
-    # Lendo dados
-    df = pd.read_csv('data/' + nome_dataset, header=None)
-
-    # Conversão para numpy
-    dados = df.to_numpy()
-    # Separando rótulos dos dados
-    ultima_coluna = dados.shape[1] - 1
-    rotulos = np.array(dados[:,ultima_coluna], dtype='int64')
-    dados = np.array(dados[:,:ultima_coluna])
-    # Pegar classes
-    classes = np.unique(rotulos)
-
-    # Normalizar dados
-    dados = normalizar_dados(nome_dataset, dados)
-
-    # medida_distancia = 'euclidean'
-    medida_distancia = definir_medida_distancia(nome_dataset)
-    matriz_distancias = gerar_matriz_distancias(dados, dados, medida_distancia)
-   
-    # Usado no RMGT
-    omega = retornar_omega(classes)
+def main():
     
-    del df
-    # 2 - Para cada valor de K
-    for k in K:
-        # Usado no RBF
-        sigma = retornar_sigma(matriz_distancias, k)
+    datasets = [
+        "ace_ECFP_4.data",
+        "ace_ECFP_6.data",
+        "analcatdata_authorship-458.data",
+        "armstrong2002v1.data",
+        "articles_1442_5.data",
+        "articles_1442_80.data",
+        "autoPrice.data",
+        "banknote-authentication.data",
+        "cardiotocography.data",
+        "chowdary2006.data",
+        "chscase_geyser1.data",
+        "cox2_ECFP_6.data",
+        "dhfr_ECFP_4.data",
+        "dhfr_ECFP_6.data",
+        "diggle_table.data",
+        "fontaine_ECFP_4.data",
+        "fontaine_ECFP_6.data",
+        "gordon2002.data",
+        "iris.data",
+        "m1_ECFP_4.data",
+        "m1_ECFP_6.data",
+        "mfeat-factors.data",
+        "mfeat-karhunen.data",
+        "seeds.data",
+        "segmentation-normcols.data",
+        "semeion.data",
+        "stock.data",
+        "transplant.data",
+        "wdbc.data",
+        "wine-187.data",
+        "yeast_Galactose.data"
+    ]
 
-        # 3 - Para cada algoritmo de adjacencia
-        for adjacencia in Adjacencia:
-            # Gerar matriz de adjacencia
-            matriz_adjacencias = gerar_matriz_adjacencias(dados, matriz_distancias, k, adjacencia)
+    K = [2, 4, 6, 8, 10, 12, 14, 16]
 
-            # 4 - Para cada ponderação
-            for ponderacao in Ponderacao:
-                # Gerar matriz pesos
-                matriz_pesos = gerar_matriz_pesos(dados, matriz_adjacencias , matriz_distancias, sigma, k, ponderacao)
+    Adjacencia = ["mutKNN", "symKNN", "symFKNN", "MST"]
 
-                simetrica, conectado = checar_matrix_adjacencias(matriz_pesos)
-                
-                # 5 - Para cada quantidade de rotulos
-                for r in Quantidade_rotulos:
+    Ponderacao = ["RBF", "HM", "LLE"]
 
-                    #Gerar os seeds
-                    seeds = random.sample(range(1, 200), Quantidade_experimentos )
+    Quantidade_rotulos = [0.02, 0.05, 0.08, 0.1]
 
-                    # 6 - Quantidade de experimentos
-                    for e in range(Quantidade_experimentos):
+    Quantidade_experimentos = 30
 
-                        # Retirar quantidade de rotulos
-                        rotulos_semissupervisionado = retirar_rotulos(rotulos, r, classes, seeds[e])
+    Propagacao = ["GRF", "RMGT", "LGC", "LapRLS", "LapSVM"]
+    
+    teste(datasets, K, Adjacencia, Ponderacao, Quantidade_rotulos, Quantidade_experimentos, Propagacao)
 
-                        posicoes_rotulos, ordemObjetos = ordem_rotulos_primeiro(rotulos_semissupervisionado)
 
-                        # Extracao das submatrizes da matriz laplaciana
-                        L, LRotulado, LNaoRotuladoRotulado, LNaoRotulado, L_normalizada = divisao_L(matriz_pesos, posicoes_rotulos, ordemObjetos)
-                        
-                        matriz_rotulos = one_hot(rotulos_semissupervisionado)
-                        yl = matriz_rotulos[posicoes_rotulos,:]
-
-                        #7 - Para cada algoritmo de classificação semi
-                        for propagacao in Propagacao:
-                            # Propagar rotulos
-                            lambda_k = 0.1
-                            lambda_u = 0.1
-                            # Usado no LGC
-                            parametro_regularizacao = 0.99
-                            rotulos_propagados = propagar(dados, L, posicoes_rotulos, ordemObjetos, LRotulado, LNaoRotuladoRotulado, LNaoRotulado, L_normalizada, yl, rotulos_semissupervisionado, matriz_rotulos, classes, medida_distancia, k, lambda_k, lambda_u, omega, parametro_regularizacao, propagacao)
-
-                            # Usar medidas de qualidade
-                            acuracia, f_measure, nRotulos = medidas_qualidade(posicoes_rotulos, ordemObjetos, rotulos, rotulos_propagados)
-
-                            # Gravar tempo que levou
-                            fim = time.time()
-                            tempo = (fim - inicio) / 60
-
-                            # gravar resultado em uma linha usando pandas
-                            gravar_resultados(test_ID, nome_dataset, k, adjacencia, simetrica, conectado, ponderacao, r, e, propagacao, seeds[e], tempo, nRotulos, acuracia, f_measure)
-
-                            print("test_ID: ", test_ID)
-
-                            test_ID += 1
-fim_geral = time.time()
-tempo_geral = fim_geral - inicio_geral
-print("Tempo geral execução (min): ", tempo_geral/60)
+if __name__ == "__main__":
+    main()
--- app/teste.py
+++ app/teste.py
@@ -12,112 +12,98 @@ from utils import ordem_rotulos_primeiro, divisao_L, gravar_resultados, definir_
 from utils import normalizar_dados, retornar_sigma, retornar_omega, checar_matrix_adjacencias
 from processar_rotulos import one_hot
 
-datasets = [
-    "m1_ECFP_6.data",
-]
+def teste(datasets, K, Adjacencia, Ponderacao, Quantidade_rotulos, Quantidade_experimentos, Propagacao):
 
-K = [2, 4, 6, 8, 10, 12, 14, 16]
+    test_ID = 0
 
-Adjacencia = ["mutKNN", "symKNN", "symFKNN", "MST"]
+    inicio_geral = time.time()
+    # 1 - Para cada dataset
+    for nome_dataset in datasets:
+        inicio = time.time()
+        print("Dataset: ", nome_dataset)
+        # Lendo dados
+        df = pd.read_csv('data/' + nome_dataset, header=None)
 
-Ponderacao = ["RBF", "HM", "LLE"]
+        # Conversão para numpy
+        dados = df.to_numpy()
+        # Separando rótulos dos dados
+        ultima_coluna = dados.shape[1] - 1
+        rotulos = np.array(dados[:,ultima_coluna], dtype='int64')
+        dados = np.array(dados[:,:ultima_coluna])
+        # Pegar classes
+        classes = np.unique(rotulos)
 
-Quantidade_rotulos = [0.02, 0.05, 0.08, 0.1]
+        # Normalizar dados
+        dados = normalizar_dados(nome_dataset, dados)
 
-Quantidade_experimentos = 1
-
-Propagacao = ["GRF", "RMGT", "LGC", "LapRLS", "LapSVM"]
-
-test_ID = 0
-
-inicio_geral = time.time()
-# 1 - Para cada dataset
-for nome_dataset in datasets:
-    inicio = time.time()
-    print("Dataset: ", nome_dataset)
-    # Lendo dados
-    df = pd.read_csv('data/' + nome_dataset, header=None)
-
-    # Conversão para numpy
-    dados = df.to_numpy()
-    # Separando rótulos dos dados
-    ultima_coluna = dados.shape[1] - 1
-    rotulos = np.array(dados[:,ultima_coluna], dtype='int64')
-    dados = np.array(dados[:,:ultima_coluna])
-    # Pegar classes
-    classes = np.unique(rotulos)
-
-    # Normalizar dados
-    dados = normalizar_dados(nome_dataset, dados)
-
-    # medida_distancia = 'euclidean'
-    medida_distancia = definir_medida_distancia(nome_dataset)
-    matriz_distancias = gerar_matriz_distancias(dados, dados, medida_distancia)
-   
-    # Usado no RMGT
-    omega = retornar_omega(classes)
+        # medida_distancia = 'euclidean'
+        medida_distancia = definir_medida_distancia(nome_dataset)
+        matriz_distancias = gerar_matriz_distancias(dados, dados, medida_distancia)
     
-    del df
-    # 2 - Para cada valor de K
-    for k in K:
-        # Usado no RBF
-        sigma = retornar_sigma(matriz_distancias, k)
+        # Usado no RMGT
+        omega = retornar_omega(classes)
+        
+        del df
+        # 2 - Para cada valor de K
+        for k in K:
+            # Usado no RBF
+            sigma = retornar_sigma(matriz_distancias, k)
 
-        # 3 - Para cada algoritmo de adjacencia
-        for adjacencia in Adjacencia:
-            # Gerar matriz de adjacencia
-            matriz_adjacencias = gerar_matriz_adjacencias(dados, matriz_distancias, k, adjacencia)
+            # 3 - Para cada algoritmo de adjacencia
+            for adjacencia in Adjacencia:
+                # Gerar matriz de adjacencia
+                matriz_adjacencias = gerar_matriz_adjacencias(dados, matriz_distancias, k, adjacencia)
 
-            # 4 - Para cada ponderação
-            for ponderacao in Ponderacao:
-                # Gerar matriz pesos
-                matriz_pesos = gerar_matriz_pesos(dados, matriz_adjacencias , matriz_distancias, sigma, k, ponderacao)
+                # 4 - Para cada ponderação
+                for ponderacao in Ponderacao:
+                    # Gerar matriz pesos
+                    matriz_pesos = gerar_matriz_pesos(dados, matriz_adjacencias , matriz_distancias, sigma, k, ponderacao)
 
-                simetrica, conectado = checar_matrix_adjacencias(matriz_pesos)
-                
-                # 5 - Para cada quantidade de rotulos
-                for r in Quantidade_rotulos:
+                    simetrica, conectado = checar_matrix_adjacencias(matriz_pesos)
+                    
+                    # 5 - Para cada quantidade de rotulos
+                    for r in Quantidade_rotulos:
 
-                    #Gerar os seeds
-                    seeds = random.sample(range(1, 200), Quantidade_experimentos )
+                        #Gerar os seeds
+                        seeds = random.sample(range(1, 200), Quantidade_experimentos )
 
-                    # 6 - Quantidade de experimentos
-                    for e in range(Quantidade_experimentos):
+                        # 6 - Quantidade de experimentos
+                        for e in range(Quantidade_experimentos):
 
-                        # Retirar quantidade de rotulos
-                        rotulos_semissupervisionado = retirar_rotulos(rotulos, r, classes, seeds[e])
+                            # Retirar quantidade de rotulos
+                            rotulos_semissupervisionado = retirar_rotulos(rotulos, r, classes, seeds[e])
 
-                        posicoes_rotulos, ordemObjetos = ordem_rotulos_primeiro(rotulos_semissupervisionado)
+                            posicoes_rotulos, ordemObjetos = ordem_rotulos_primeiro(rotulos_semissupervisionado)
 
-                        # Extracao das submatrizes da matriz laplaciana
-                        L, LRotulado, LNaoRotuladoRotulado, LNaoRotulado, L_normalizada = divisao_L(matriz_pesos, posicoes_rotulos, ordemObjetos)
-                        
-                        matriz_rotulos = one_hot(rotulos_semissupervisionado)
-                        yl = matriz_rotulos[posicoes_rotulos,:]
+                            # Extracao das submatrizes da matriz laplaciana
+                            L, LRotulado, LNaoRotuladoRotulado, LNaoRotulado, L_normalizada = divisao_L(matriz_pesos, posicoes_rotulos, ordemObjetos)
+                            
+                            matriz_rotulos = one_hot(rotulos_semissupervisionado)
+                            yl = matriz_rotulos[posicoes_rotulos,:]
 
-                        #7 - Para cada algoritmo de classificação semi
-                        for propagacao in Propagacao:
-                            # Propagar rotulos
-                            lambda_k = 0.1
-                            lambda_u = 0.1
-                            # Usado no LGC
-                            parametro_regularizacao = 0.99
-                            rotulos_propagados = propagar(dados, L, posicoes_rotulos, ordemObjetos, LRotulado, LNaoRotuladoRotulado, LNaoRotulado, L_normalizada, yl, rotulos_semissupervisionado, matriz_rotulos, classes, medida_distancia, k, lambda_k, lambda_u, omega, parametro_regularizacao, propagacao)
+                            #7 - Para cada algoritmo de classificação semi
+                            for propagacao in Propagacao:
+                                # Propagar rotulos
+                                lambda_k = 0.1
+                                lambda_u = 0.1
+                                # Usado no LGC
+                                parametro_regularizacao = 0.99
+                                rotulos_propagados = propagar(dados, L, posicoes_rotulos, ordemObjetos, LRotulado, LNaoRotuladoRotulado, LNaoRotulado, L_normalizada, yl, rotulos_semissupervisionado, matriz_rotulos, classes, medida_distancia, k, lambda_k, lambda_u, omega, parametro_regularizacao, propagacao)
 
-                            # Usar medidas de qualidade
-                            acuracia, f_measure, nRotulos = medidas_qualidade(posicoes_rotulos, ordemObjetos, rotulos, rotulos_propagados)
+                                # Usar medidas de qualidade
+                                acuracia, f_measure, nRotulos = medidas_qualidade(posicoes_rotulos, ordemObjetos, rotulos, rotulos_propagados)
 
-                            # Gravar tempo que levou
-                            fim = time.time()
-                            tempo = (fim - inicio) / 60
+                                # Gravar tempo que levou
+                                fim = time.time()
+                                tempo = (fim - inicio) / 60
 
-                            # gravar resultado em uma linha usando pandas
-                            gravar_resultados(test_ID, nome_dataset, k, adjacencia, simetrica, conectado, ponderacao, r, e, propagacao, seeds[e], tempo, nRotulos, acuracia, f_measure)
+                                # gravar resultado em uma linha usando pandas
+                                gravar_resultados(test_ID, nome_dataset, k, adjacencia, simetrica, conectado, ponderacao, r, e, propagacao, seeds[e], tempo, nRotulos, acuracia, f_measure)
 
-                            #print("test_ID: ", test_ID)
+                                #print("test_ID: ", test_ID)
 
-                            test_ID += 1
-fim_geral = time.time()
-tempo_geral = fim_geral - inicio_geral
-#print("test_ID: ", test_ID)
-#print("Tempo geral execução (min): ", tempo_geral/60)
+                                test_ID += 1
+    fim_geral = time.time()
+    tempo_geral = fim_geral - inicio_geral
+    #print("test_ID: ", test_ID)
+    #print("Tempo geral execução (min): ", tempo_geral/60)
