b654d66a00cc57dd101893302f6fed05426e5a51
Leonardo Macedo Freire <lmacedo.freire@gmail.com>
2024-10-24 14:53:16 -0300

    chore: Simplifica main

--- app/main.py
+++ app/main.py
@@ -1,12 +1,3 @@
-import sys
-import os
-
-# Caminho absoluto para o diretório que contém o módulo
-caminho_diretorio = os.path.join(os.path.dirname(__file__), 'algoritmos-dissertacao', 'app')
-# Adiciona o diretório ao sys.path
-sys.path.append(caminho_diretorio)
-
-# Agora podemos importar o módulo diretamente
 from teste import teste
 
 
