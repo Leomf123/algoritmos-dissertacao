53cae453d65194a7e0cf53c142d946431e48d8e2
Leonardo Macedo Freire <lmacedo.freire@gmail.com>
2024-12-29 17:21:41 -0300

    chore: Otimiza funcoes dividir L e pegar posicoes

--- app/utils.py
+++ app/utils.py
@@ -62,45 +62,41 @@ def primMST(grafo):
 
 def ordem_rotulos_primeiro(rotulos):
 
-  #pegar posições existe rotulo
-  posicoes_rotulos =  np.where( rotulos != 0)[0]
+    # Máscara para identificar elementos rotulados
+    mascara_rotulos = rotulos != 0
 
-  # Reordenar as posições para os rotulados vim primeiro
-  ordemObjetos = np.arange(rotulos.shape[0])
-  # Retira dos indices os que são rotulados
-  ordemObjetos = np.setdiff1d(ordemObjetos, posicoes_rotulos)
-  # ordemObjetos será uma lista onde os indices dos objetos rotulados
-  # vem primeiro, depois o resto em ordem crescente de indice
-  ordemObjetos = np.concatenate((posicoes_rotulos,ordemObjetos))
+    # Índices de rótulos e não rótulos
+    posicoes_rotulos = np.flatnonzero(mascara_rotulos)
+    nao_rotulados = np.flatnonzero(~mascara_rotulos)
 
-  return posicoes_rotulos, ordemObjetos
+    # Combina diretamente os índices
+    ordemObjetos = np.hstack((posicoes_rotulos, nao_rotulados))
+
+    return posicoes_rotulos, ordemObjetos
 
 def divisao_L(matriz_pesos, posicoes_rotulos, ordemObjetos):
 
-    # Calculo da matriz diagonal: Uma matriz de grau de cada um dos vertices
-    D = np.zeros(matriz_pesos.shape)
-    np.fill_diagonal(D, np.sum(matriz_pesos, axis=1))
-    # Calculo da matriz laplaciana
-    L= 1.01*D - matriz_pesos
-
-    # Calculo da laplaciana normalizada
-    matriz_identidade = np.eye(matriz_pesos.shape[0])
+    n = matriz_pesos.shape[0]
+    
+    # Matriz diagonal (grau dos vértices)
+    D = np.diag(np.sum(matriz_pesos, axis=1))
+    
+    # Matriz laplaciana
+    L = 1.01 * D - matriz_pesos
+    
+    # Laplaciana normalizada
     D_inv_raiz = np.diag(1 / np.sqrt(np.diag(D)))
-    L_normalizada = 1.01*matriz_identidade - D_inv_raiz.dot(matriz_pesos.dot(D_inv_raiz))
-
-    ## Reordenacao da matriz Laplaciana, para manter os dados rotulados à frente
-    L = L[ordemObjetos,:]
+    L_normalizada = 1.01 * np.eye(n) - D_inv_raiz @ matriz_pesos @ D_inv_raiz
+    
+    # Reordenar matriz laplaciana
+    L = L[ordemObjetos, :]
     L = L[:, ordemObjetos]
-
-    # Armazenar numero de objetos rotulados, nao rotulados e total de objetos
+    
+    # Divisão em submatrizes
     nRotulado = len(posicoes_rotulos)
-    nNaoRotulado = L.shape[0]-nRotulado
-    nObjetos = L.shape[0]
-
-    # Extracao das submatrizes da matriz laplaciana
-    LRotulado = L[0:nRotulado, 0:nRotulado]
-    LNaoRotuladoRotulado = L[nRotulado:nObjetos, 0:nRotulado]
-    LNaoRotulado = L[nRotulado:nObjetos, nRotulado:nObjetos]
+    LRotulado = L[:nRotulado, :nRotulado]
+    LNaoRotuladoRotulado = L[nRotulado:, :nRotulado]
+    LNaoRotulado = L[nRotulado:, nRotulado:]
 
     return L, LRotulado, LNaoRotuladoRotulado, LNaoRotulado, L_normalizada
 
